78a2de74083ebb972ce0e4f6f1bb6c83cbbc61c8
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-04-21 23:15:54 +0200

    changi

--- src/stage/dummy.c
+++ src/stage/dummy.c
@@ -6,14 +6,43 @@
 
 #include "dummy.h"
 
+#include "../archive.h"
 #include "../mem.h"
+#include "../stage.h"
 
 //Dummy background structure
 typedef struct
 {
 	//Stage background base structure
 	StageBack back;
+
+
+
+
+    Gfx_Tex tex_back1; //Curtains
 } Back_Dummy;
+
+//Week 1 background functions
+void Back_Dummy_DrawBG(StageBack *back)
+{
+	Back_Dummy *this = (Back_Dummy*)back;
+	
+	fixed_t fx, fy;
+	
+	//Draw curtains
+	fx = stage.camera.x;
+	fy = stage.camera.y;
+	
+	RECT tex_src = {0, 0, 255, 255};
+	RECT_FIXED tex_dst = {
+		FIXED_DEC(0,1) - fx,
+		FIXED_DEC(0,1) - fy,
+		FIXED_DEC(256,1),
+		FIXED_DEC(256,1)
+	};
+	
+	Gfx_DrawTexRotate(&this->tex_back1, &tex_src, &tex_dst, stage.camera.bzoom, 90);
+}
 
 //Dummy background functions
 void Back_Dummy_Free(StageBack *back)
@@ -34,8 +63,13 @@ StageBack *Back_Dummy_New(void)
 	//Set background functions
 	this->back.draw_fg = NULL;
 	this->back.draw_md = NULL;
-	this->back.draw_bg = NULL;
+	this->back.draw_bg = Back_Dummy_DrawBG;
 	this->back.free = Back_Dummy_Free;
+	
+    //Load background textures
+	IO_Data arc_back = IO_Read("\\WEEK1\\BACK.ARC;1");
+	Gfx_LoadTex(&this->tex_back1, Archive_Find(arc_back, "back1.tim"), 0);
+	Mem_Free(arc_back);
 	
 	//Use non-pitch black background
 	Gfx_SetClear(62, 48, 64);
